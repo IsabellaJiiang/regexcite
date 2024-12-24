3f488cca7acdc103c030e60fc7263f806baa4006
IsabellaJiiang <Isabella.jyn@gmail.com>
2024-12-23 20:13:16 -0800

    added knitted README.html and adjust the error in test-str_split_one.R

--- tests/testthat/test-str_split_one.R
+++ tests/testthat/test-str_split_one.R
@@ -7,6 +7,6 @@ test_that("str_split_one() errors if input length > 1", {
 })
 
 test_that("str_split_one() exposes features of stringr::str_split()",{
-  expected_equal(str_split_one("a,b,c",",",n=2), c("a","b,c"))
-  expected_equal(str_split_one("a.b", stringr::fixed(".")), c("a","b"))
+  expect_equal(str_split_one("a,b,c",",",n=2), c("a","b,c"))
+  expect_equal(str_split_one("a.b", stringr::fixed(".")), c("a","b"))
 })
